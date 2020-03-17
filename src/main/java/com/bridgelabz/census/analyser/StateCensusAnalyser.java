e2da7f0fd32fe1ce9f9a7708faf44e1a974e125f
Nileshlad <ladnilesh1111@gmail.com>
2020-03-17 12:14:20 +0530

    Added: To welcome indian states census analyser problem

---
+++ src/main/java/com/bridgelabz/census/analyser/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.census.analyser;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome Indian States Census Analyser Problem");
+    }
+}
---
+++ src/test/java/com/bridgelabz/census/analyser/test/StateCensusAnalyserTest.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.census.analyser.test;
+
+public class StateCensusAnalyserTest {
+}
