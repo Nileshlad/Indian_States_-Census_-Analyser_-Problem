6f203eb22db4b7dc53ee26a1093e6ddb3c0751c3
Nileshlad <ladnilesh1111@gmail.com>
2020-03-18 10:24:41 +0530

    TC 1.1 To check the number of record matches

--- src/main/java/com/bridgelabz/census/analyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/census/analyser/StateCensusAnalyser.java
@@ -1,7 +1,43 @@
 package com.bridgelabz.census.analyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome Indian States Census Analyser Problem");
+
+    //METHOD TO LOAD THE CSV FILE AND GET
+    public int loadIndiaCensusData(String csvFilePath)  {
+        int recordCount=0;
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
+        {
+            CsvToBeanBuilder<CSVstateCensus> csvToBeanBuilder = new CsvToBeanBuilder<CSVstateCensus>(reader);
+            csvToBeanBuilder.withType(CSVstateCensus.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CSVstateCensus> csvToBean = csvToBeanBuilder
+                    .build();
+            Iterator<CSVstateCensus> censusCSVIterator = csvToBean.iterator();
+            while (censusCSVIterator.hasNext()) {
+                System.out.print(recordCount+++"  ");
+                CSVstateCensus censusCSV = censusCSVIterator.next();
+                System.out.print("state:"+censusCSV.getState()+", ");
+                System.out.print("population"+censusCSV.getPopulation()+", ");
+                System.out.print("area"+censusCSV.getAreaInSqKm()+", ");
+                System.out.print("density"+censusCSV.getDensityPerSqKm()+", ");
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return recordCount;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("Welcome to Indian States Census Analyser Problem");
     }
 }
+
--- src/test/java/com/bridgelabz/census/analyser/test/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/census/analyser/test/StateCensusAnalyserTest.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.census.analyser.test;
 
-public class StateCensusAnalyserTest {
+import com.bridgelabz.census.analyser.StateCensusAnalyser;
+import org.junit.Assert;
+        import org.junit.Test;
+
+public class StateCensusAnalyserTest{
+
+    //CONSTANT
+    private static final String STATE_CENSUS_DATA_PATH = "./src/test/resources/StateCensusData.csv";
+
+    //OBJECT CREATION
+    StateCensusAnalyser censusAnalyserProblem = new StateCensusAnalyser();
+
+    @Test
+    public void givenIndianCensusCsvFile_WhenProper_ShouldReturnCorrectRecord() {
+        int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(STATE_CENSUS_DATA_PATH);
+        Assert.assertEquals(29,numberOfRecord);
+    }
 }
