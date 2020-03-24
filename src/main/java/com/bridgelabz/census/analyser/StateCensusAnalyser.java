11973466cd8312c24e832561203be4f043392230
Nileshlad <ladnilesh1111@gmail.com>
2020-03-24 18:12:02 +0530

    Refactored: To dry principle was voilated to extract data from opencsv

--- src/main/java/com/bridgelabz/census/analyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/census/analyser/StateCensusAnalyser.java
@@ -9,54 +9,76 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.regex.Pattern;
-
 public class StateCensusAnalyser {
-
     //CONSTANT
     private static final String PATTERN_FOR_CSV_FILE = "^[a-zA-Z0-9./_@]*[.]+[c][s][v]$";
+    //METHOD TO LOAD THE CSV FILE AND GET
+    public int loadIndiaCensusData(String csvFilePath) throws StateCensusAnalyserException {
+        int recordCount=0;
+        String extension = getFileExtension(csvFilePath);
+        if (!Pattern.matches(PATTERN_FOR_CSV_FILE,extension))
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.NO_SUCH_TYPE,"No such a type");
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
+        {
+            Iterator<IndianStateCode> censusCSVIterator = this.getCsvFileIterator(reader,IndianStateCode.class);
+            while (censusCSVIterator.hasNext()) {
+                recordCount++;
+                IndianStateCode censusCSV = censusCSVIterator.next();
+            }
+        } catch (RuntimeException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.WRONG_DELIMITER_OR_HEADER,"Delimiter or header not found");
+        }catch (NoSuchFileException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.FILE_NOT_FOUND,"File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }return recordCount;
+    }
 
     //METHOD TO LOAD THE CSV FILE AND GET
     public int loadIndianStateCodeData(String csvFilePath) throws StateCensusAnalyserException {
         //LOCAL VARIABLE
-        int recordCount = 0;
+        int recordCount=0;
         String extension = getFileExtension(csvFilePath);
-        if (!Pattern.matches(PATTERN_FOR_CSV_FILE, extension))
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.NO_SUCH_TYPE, "No such a type");
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBean<IndianStateCode> csvToBean = new CsvToBeanBuilder(reader)
-                    .withType(IndianStateCode.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            Iterator<IndianStateCode> statesCSVIterator = csvToBean.iterator();
+        if (!Pattern.matches(PATTERN_FOR_CSV_FILE,extension))
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.NO_SUCH_TYPE,"No such a type");
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
+        {Iterator<IndianStateCode>statesCSVIterator = this.getCsvFileIterator(reader,IndianStateCode.class);
             while (statesCSVIterator.hasNext()) {
                 IndianStateCode censusCSV = statesCSVIterator.next();
-                recordCount++;
-                System.out.print("SrNo: " + censusCSV.getSrNo() + ", ");
-                System.out.print("state: " + censusCSV.getState() + ", ");
-                System.out.print("Name: " + censusCSV.getName() + ", ");
-                System.out.print("TIN: " + censusCSV.getTin() + ", ");
-                System.out.print("StateCode: " + censusCSV.getStateCode() + ", ");
-                System.out.println();
+                ++recordCount;
             }
         } catch (RuntimeException e) {
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.WRONG_DELIMITER_OR_HEADER, "No such delimiter and header");
-        } catch (NoSuchFileException e) {
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.FILE_NOT_FOUND, "File not found");
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.WRONG_DELIMITER_OR_HEADER,"No such delimiter and header");
+        }catch (NoSuchFileException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusAnalyserCustomExceptionType.FILE_NOT_FOUND,"File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return recordCount;
     }
-
-
     //METHOD TO GET EXTENSION OF CSV FILE
     private static String getFileExtension(String file) {
         String extension = "";
+        try {
+            if (file != null) {
+                extension = file.substring(file.lastIndexOf("."));
+            }
+        } catch (Exception e) {
+            extension = "";
+        }
         return extension;
     }
 
-    public static void main(String[] args) {
+    //METHOD TO GET CSV ITERATOR
+    private <E> Iterator<E> getCsvFileIterator(Reader reader,Class<E> csvClass) {
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.iterator();
+    }
+
+    public static void main(String[] args)  {
         System.out.println("Welcome to Indian States Census Analyser Problem");
     }
 }
-
